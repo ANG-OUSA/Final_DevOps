8e30dea5cfc6d353ccb7ed7fd073ce99e7b60ee3
ANG-OUSA <angousa168@gmail.com>
2024-06-14 16:24:24 +0700

    test2

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -7,19 +7,19 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Book;
 
-class BookTest extends TestCase
-{
-    use RefreshDatabase;
+// class BookTest extends TestCase
+// {
+//     use RefreshDatabase;
 
-    /** @test */
-    public function it_creates_a_book()
-    {
-        $book = Book::create([
-            'title' => 'The Great Gatsby',
-            'author' => 'F. Scott Fitzgerald',
-            'isbn' => '9780743273565',
-        ]);
+//     /** @test */
+//     public function it_creates_a_book()
+//     {
+//         $book = Book::create([
+//             'title' => 'The Great Gatsby',
+//             'author' => 'F. Scott Fitzgerald',
+//             'isbn' => '9780743273565',
+//         ]);
 
-        $this->assertTrue($book->exists());
-    }
-}
+//         $this->assertTrue($book->exists());
+//     }
+// }
