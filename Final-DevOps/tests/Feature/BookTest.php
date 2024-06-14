a2efec235c9c487b1d2cec285e20462971ba3392
ANG-OUSA <angousa168@gmail.com>
2024-06-14 14:58:48 +0700

    final_exercise2

--- Final-DevOps/tests/Feature/BookTest.php
+++ Final-DevOps/tests/Feature/BookTest.php
@@ -5,36 +5,21 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
 use App\Models\Book;
-use function PHPUnit\Framework\assertTrue;
 
-test('create book', function () {
-    $book = Book::create([
-        'title' => 'The Great Gatsby',
-        'author' => 'F. Scott Fitzgerald',
-        'isbn' => '9780743273565',
-    ]);
-    assertTrue($book->exists());
-});
+class BookTest extends TestCase
+{
+    use RefreshDatabase;
 
-// class BookTest extends TestCase
-// {
-//     /**
-//      * A basic feature test example.
-//      */
-//     public function test_example(): void
-//     {
-//         $response = $this->get('/');
+    /** @test */
+    public function it_creates_a_book()
+    {
+        $book = Book::create([
+            'title' => 'The Great Gatsby',
+            'author' => 'F. Scott Fitzgerald',
+            'isbn' => '9780743273565',
+        ]);
 
-//         $response->assertStatus(200);
-//     }
-//     test('create book', function () {
-//         $book = Book::create([
-//             'title' => 'The Great Gatsby',
-//             'author' => 'F. Scott Fitzgerald',
-//             'isbn' => '9780743273565',
-//         ]);
-//         assertTrue($book->exists());
-//     });
-// }
+        $this->assertTrue($book->exists());
+    }
+}
